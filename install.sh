2cdcd2ee60df899a31ba0c5382d8856abeb90bb3
YJC <y3332664073@gmail.com>
2023-08-05 21:06:22 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+echo "Halo v2.8.0博客一键搭建脚本"
+echo "脚本作者：知性"
+echo "GitHub开源地址：https://github.com/aizhiqian/halo-on-replit"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/aizhiqian/halo-on-replit/raw/main/.replit
+wget -O replit.nix https://github.com/aizhiqian/halo-on-replit/raw/main/replit.nix
+cd ..
+wget -O main.sh https://github.com/aizhiqian/halo-on-replit/raw/main/main.sh
+wget -O halo.jar https://github.com/aizhiqian/halo-on-replit/releases/download/2.8.0/halo.jar
+cp -r build/.replit . && cp -r build/replit.nix .
+rm -rf build/
+echo "首次搭建需要在Secrets设置环境变量"
+echo "设置账号和密码的环境变量，点Run启动Halo项目！！！"
+echo "Halo博客v2.8.0一键搭建脚本"
+echo "脚本作者：知性"
+echo "GitHub开源地址：https://github.com/aizhiqian/halo-on-replit"
+rm -rf README.md
