c8bb8c6a9720fb72bb4c28a62429778b2fa46d91
YJC <y3332664073@gmail.com>
2023-09-01 15:11:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ wget -O .replit https://raw.githubusercontent.com/aizhiqian/halo-on-replit/main/
 wget -O replit.nix https://raw.githubusercontent.com/aizhiqian/halo-on-replit/main/replit.nix
 cd ..
 wget -O main.sh https://raw.githubusercontent.com/aizhiqian/halo-on-replit/main/main.sh
-wget -O halo.jar https://github.com/aizhiqian/halo-on-replit/releases/download/2.8.0/halo.jar
+wget -O update.sh https://raw.githubusercontent.com/aizhiqian/halo-on-replit/main/update.sh
+wget -O halo.jar https://github.com/aizhiqian/halo-on-replit/releases/latest/download/halo.jar
 cp -r build/.replit . && cp -r build/replit.nix .
 rm -rf build/
 echo "首次搭建需要在Secrets设置环境变量"
