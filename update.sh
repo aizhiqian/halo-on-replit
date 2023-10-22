826cb5dbb0d202eb374cbdbcd763248252c56618
YJC <y3332664073@gmail.com>
2023-10-22 23:28:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 echo "Halo一键更新脚本"
 echo "脚本作者：知性"
 echo "GitHub开源地址：https://github.com/aizhiqian/halo-on-replit"
-rm -rf halo.jar && wget -O halo.jar https://github.com/aizhiqian/halo-on-replit/releases/latest/download/halo.jar
-echo "更新至 Halo 最新版，更新完毕！"
+rm -rf halo.jar
+if [ ! -e halo.jar ];then
+    if [ $# -eq 1 ]; then
+      # 下载指定版本
+      version=$1
+    else
+      # 下载最新版
+      version="$(curl -fsSLI -o /dev/null -w "%{url_effective}" https://github.com/aizhiqian/halo-on-replit/releases/latest)"
+      version="${version#https://github.com/aizhiqian/halo-on-replit/releases/tag/}"
+    fi
+    wget -O halo.jar https://github.com/aizhiqian/halo-on-replit/releases/download/${version}/halo.jar
+fi
+echo "Halo 已更新至 ${version}，更新完毕！"
 echo "点击Run按钮重新启动本项目！！！"
